8d79d8bf452654156b53a45dda08e0097f9d8bc5
Ezequiel <cruzezequiel81@gmail.com>
2025-02-11 19:45:41 -0500

    Super simple pyhton project

---
+++ project.py
@@ -0,0 +1,11 @@
+name = input("Please Enter Your Name:")
+
+print("Hello " + name + "!")
+
+age_str = input("What year were you born " + name + ":")
+
+age = 2025 - int(age_str)
+
+
+
+print("Oh so you are: " + str(age) + " years old")
